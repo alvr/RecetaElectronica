4456de547a3918942d87d1115a0c216b19165ca8
alvr <alvaro.sg93@gmail.com>
2014-12-15 23:46:31 +0100

    Cambio en la declaración acorde con el nombre del archivo.

--- Intro.vb
+++ Intro.vb
@@ -1,6 +1,6 @@
 ﻿Public NotInheritable Class Intro
 
-    Private Sub Splash_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+    Private Sub Intro_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         If My.Application.Info.Title <> "" Then
             lbTitulo.Text = My.Application.Info.Title
         Else
@@ -8,7 +8,6 @@
         End If
 
         lbVersion.Text = System.String.Format(lbVersion.Text, My.Application.Info.Version.Major, My.Application.Info.Version.Minor, My.Application.Info.Version.Build, My.Application.Info.Version.Revision)
-
         lbCopyright.Text = My.Application.Info.Copyright
     End Sub
 End Class
