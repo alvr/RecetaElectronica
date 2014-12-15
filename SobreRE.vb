d4d165a6c1862b7c8761ad24753a439a77cabc3a
alvr <alvaro.sg93@gmail.com>
2014-12-15 23:52:07 +0100

    Cambio en una variable y en la declaración.

--- SobreRE.vb
+++ SobreRE.vb
@@ -1,13 +1,13 @@
 ﻿Public NotInheritable Class SobreRE
 
-    Private Sub About_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        Dim ApplicationTitle As String
+    Private Sub SobreRE_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim TituloAplicacion As String
         If My.Application.Info.Title <> "" Then
-            ApplicationTitle = My.Application.Info.Title
+            TituloAplicacion = My.Application.Info.Title
         Else
-            ApplicationTitle = System.IO.Path.GetFileNameWithoutExtension(My.Application.Info.AssemblyName)
+            TituloAplicacion = System.IO.Path.GetFileNameWithoutExtension(My.Application.Info.AssemblyName)
         End If
-        Me.Text = String.Format("Acerca de {0}", ApplicationTitle)
+        Me.Text = String.Format("Acerca de {0}", TituloAplicacion)
         Me.lbProducto.Text = My.Application.Info.ProductName
         Me.lbVersion.Text = String.Format("Versión {0}", My.Application.Info.Version.ToString)
         Me.lbCopyright.Text = My.Application.Info.Copyright
@@ -15,7 +15,7 @@
         Me.tbDescripcion.Text = My.Application.Info.Description
     End Sub
 
-    Private Sub OKButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAceptar.Click
+    Private Sub btnAceptar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAceptar.Click
         Me.Close()
     End Sub
 End Class
